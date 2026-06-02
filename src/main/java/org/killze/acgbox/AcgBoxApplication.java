9c0beefbe3903b72b79c1bd77643eaed2c689af6
KilLze <kirito24177@163.com>
2026-06-02 18:19:14 +0800

    初始化

---
+++ src/main/java/org/killze/acgbox/AcgBoxApplication.java
@@ -0,0 +1,13 @@
+package org.killze.acgbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcgBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcgBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/killze/acgbox/AcgBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.killze.acgbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcgBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
