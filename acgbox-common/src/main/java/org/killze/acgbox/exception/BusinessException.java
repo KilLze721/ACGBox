73f929d30681ae3edcb5958bef50ce98a274c30c
KilLze <kirito24177@163.com>
2026-06-05 03:42:06 +0800

    添加全局异常处理器

---
+++ acgbox-common/src/main/java/org/killze/acgbox/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package org.killze.acgbox.exception;
+
+/**
+ * 业务异常类
+ */
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ acgbox-common/src/main/java/org/killze/acgbox/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package org.killze.acgbox.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.killze.acgbox.result.Result;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 自定义业务异常
+     */
+    @ExceptionHandler(BusinessException.class)
+    public Result<Void> handleBusinessException(BusinessException e) {
+        return Result.error(e.getMessage());
+    }
+
+    /**
+     * 参数校验异常
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result<Void> handleValidationException(MethodArgumentNotValidException e) {
+        String msg = e.getBindingResult().getFieldError().getDefaultMessage();
+        return Result.error(msg);
+    }
+
+    /**
+     * 未知异常
+     */
+    @ExceptionHandler(Exception.class)
+    public Result<Void> handleException(Exception e) {
+        log.error("系统异常", e);
+        return Result.error("系统错误，请联系管理员");
+    }
+}
